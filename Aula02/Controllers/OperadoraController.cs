d514f893a9a7bf3c37016d03dc193fbe2f2b6986
UniALFA <UniALFA@LAB-03-PC-11>
2024-08-06 22:05:36 -0300

    Adicionar arquivos de projeto.

---
+++ Aula02/Controllers/OperadoraController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Aula02.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OperadoraController : ControllerBase
+{
+    SqlConnection conexao;
+
+    public OperadoraController()
+    {
+        conexao = new SqlConnection(
+            @"Server=LAB-03-PC-11\SQLEXPRESS01; 
+            Database=AgendaFoneDb; 
+            Trusted_Connection=True;"
+        );
+
+        conexao.Open();
+    }
+}
