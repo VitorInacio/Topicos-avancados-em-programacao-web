d3871a18e495e0b47fa2548076a97546cb412788
VitorInacio <vitorinacio16@gmail.com>
2024-08-13 22:18:52 -0300

    13/08

---
+++ Primeiro Projeto/Aula02/Repositories/OperadoraRepository.cs
@@ -0,0 +1,75 @@
+﻿using System.Data.SqlClient;
+
+namespace Aula02.Repositories
+{
+
+    public class OperadoraRepository
+    {
+        SqlConnection conexao;
+
+        public OperadoraRepository()
+        {
+            conexao = new SqlConnection(
+            @"Server=LAB-01-MICRO-15\SQLEXPRESS;
+            Database=AgendaFoneDb;
+            Trusted_Connection=True;"
+        );
+            //            Encrypt=False;
+
+        }
+
+        public IList<string> BuscarTodas()
+        {
+            try
+            {
+                conexao.Open();
+
+                string sql = "SELECT * FROM TbOperadora";
+                using SqlCommand command = new SqlCommand(sql, conexao);
+                using SqlDataReader reader = command.ExecuteReader();
+                var retorno = new List<string>();
+
+                while (reader.Read())
+                {
+                    retorno.Add(reader["OpeNome"].ToString() ?? "");
+                }
+
+                return retorno;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public bool Adicionar(String nome)
+
+        {
+            try
+            {
+                conexao.Open();
+
+                string sql = "INSERT INTO TbOperadora (OpeNome) VALUES (@OpeNome)";
+
+                using SqlCommand command = new SqlCommand(sql, conexao);
+                // command.Parameters.Add("@OpeNome", SqlDbType.VarChar).Value = nome;
+                command.Parameters.AddWithValue("@OpeNome", nome);
+
+                command.ExecuteNonQuery();
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                conexao.Open();
+            }
+        }
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Aula02
+
+## Passos importantes
+
+1. Abrir SQL Server Management Studio 20
+2. Pegar o nome do servidor e colar na controller
+3. No SQL server, criar um novo banco de dados com o nome que está na controller
